73cf244b136ddb1b09aa6b5c4c4d1b0418ac1537
Ralf S. Engelschall <rse@engelschall.com>
2018-03-25 18:13:43 +0200

    a simple test suite

---
+++ test.js
@@ -0,0 +1,46 @@
+
+const HAPI       = require("hapi")
+const HAPIHeader = require("./hapi-plugin-header")
+const Request    = require("request-promise")
+
+;(async () => {
+    const server = HAPI.server({
+        host:  "127.0.0.1",
+        port:  12345,
+        debug: { request: [ "error" ] }
+    })
+
+    await server.register({
+        plugin: HAPIHeader,
+        options: {
+            "Server":        "Example/1.2.3",
+            "X-Request-Id":  (server, request, h) => request.info.id,
+            "X-External-IP": (server, request, h) => {
+                return Request.get("http://myexternalip.com/raw")
+                    .then((value) => value.replace(/\r?\n/g, ""))
+            }
+        }
+    })
+
+    server.route({
+        method:  "GET",
+        path:    "/foo",
+        handler: async (request, h) => {
+            return "OK"
+        }
+    })
+    await server.start()
+
+    let response = await server.inject({
+        method:  "GET",
+        url:     "/foo"
+    })
+    if (response.result === "OK")
+        console.log("-- internal request: /foo: OK", response.headers)
+    else
+        console.log("-- internal request: /foo: ERROR: invalid response: ", response.result)
+
+    await server.stop({ timeout: 1000 })
+    process.exit(0)
+})()
+
